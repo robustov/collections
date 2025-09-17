815eca202d741c9a137f2333eb3272ad957ea3af
robustov <yaroslavrobustov@yandex.ru>
2025-09-17 11:21:36 +0300

    minor fixes to class names regarding tests

--- lib/src/test/java/org/robustov/collections/api/ReadOnlySetTests.java
+++ lib/src/test/java/org/robustov/collections/api/ReadOnlySetTests.java
@@ -10,7 +10,7 @@ import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class ReadOnlySetAdditionalTest {
+class ReadOnlySetTests {
 
   @Test
   void equalsAndHashCodeSymmetry() {
